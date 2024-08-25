73bcf673065ab51dc79675294fe0f4d922175efd
Lucas Leocadio <lucasleocadio05@gmail.com>
2024-08-25 18:48:53 -0300

    Functions deleted and operations placed in main def

--- Exercício Banco/main.py
+++ Exercício Banco/main.py
@@ -1,10 +1,6 @@
-# Declaração de variáveis globais padrão
-nomeCliente = "Carlos Eduardo"
-extrato = 2000.00
-quantidadeSaques = 0
-
 def main():
-
+    saldo = float(input("Digite o saldo da conta: "))
+    quantidadeSaques = 0
     while True:
         print("""
         Aplicativo de Banco
@@ -13,33 +9,31 @@ def main():
         [S]aque
         [E]xtrato bancário
         """)
-        opcaoDesejada = input("O que deseja fazer?")
+        opcaoDesejada = input("Digite a opção desejada: ")
         match opcaoDesejada:
             case 'D':
-                deposito()
+                quantiaDeposito = float(input("Digite a quantidade que deseja depositar: "))
+                if quantiaDeposito > 0:
+                    saldo += quantiaDeposito
+                    print(f"Depósito realizado! O saldo atual é de {saldo}")
+                else:
+                    print("A quantia de depósito é inválida")
             case 'S':
-                saque()
+                if quantidadeSaques >= 3:
+                    print("Não é possível sacar mais dinheiro, pois o limite é de 3 saques por dia")
+                else:
+                    valorSaque = float(input("Digite a quantidade desejada para o saque: "))
+                    if valorSaque > saldo:
+                        print("O valor que deseja extrair é maior do que o próprio extrato. Tentativa inválida")
+                    elif valorSaque > 500:
+                        print("O limite de dinheiro que pode ser sacado, por vez,  é de até 500 reais")
+                    else:
+                        saldo -= valorSaque
+                        print(f"Saque realizado! A quantia atual de dinheiro é R${saldo}")
+                        quantidadeSaques += 1
             case 'E':
-                consultarExtrato()
+                print(saldo)
             case _:
                 print("Não foi possível identificar a opção. Digite novamente.")
-                
-def deposito():
-    quantia = float(input("Digite a quantidade desejada para o depósito: "))
-    if quantia > 0:
-        extrato += quantia
-    else:
-        print("A quantia é inválida.")
-
-def saque():
-    if quantidadeSaques < 3:
-        print("Não é possível sacar mais dinheiro, pois o limite é de 3 saques por dia")
-    else:
-        quantia = float(input("Digite a quantidade desejada para o saque: "))
-        if quantia > extrato:
-            print("O valor que deseja extrair é maior do que o próprio extrato. Tentativa inválida")
-        elif quantia > 500:
-            print("O limite de saques é de 3 vezes por dia, e o limite de dinheiro que pode ser sacado é de até 500 reais")
-
 
 main()
